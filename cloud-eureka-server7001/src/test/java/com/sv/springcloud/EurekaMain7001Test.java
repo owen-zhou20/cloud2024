01b21de01d5b347d2adf13aac9c3cbe0ef88c148
owen-zhou20 <78416546+owen-zhou20@users.noreply.github.com>
2024-03-04 16:20:09 +1100

    feat: add Eureka

---
+++ cloud-eureka-server7001/src/test/java/com/sv/springcloud/EurekaMain7001Test.java
@@ -0,0 +1,20 @@
+package com.sv.springcloud;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple EurekaMain7001.
+ */
+public class EurekaMain7001Test
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
