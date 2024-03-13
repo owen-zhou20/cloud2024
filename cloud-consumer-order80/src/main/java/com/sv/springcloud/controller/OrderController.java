64886292fca78c11d903cdc3d08b7ee1ac28af52
owen-zhou20 <78416546+owen-zhou20@users.noreply.github.com>
2024-03-13 15:00:55 +1100

    feat: add MyLB in Eureka.

--- cloud-consumer-order80/src/main/java/com/sv/springcloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/sv/springcloud/controller/OrderController.java
@@ -3,6 +3,9 @@ package com.sv.springcloud.controller;
 import com.sv.springcloud.entities.CommonResult;
 import com.sv.springcloud.entities.Payment;
 import lombok.extern.slf4j.Slf4j;
+import com.sv.springcloud.lb.LoadBalancer;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,6 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.Resource;
+import java.net.URI;
+import java.util.List;
 
 /**
  * @author Owen
@@ -24,6 +29,12 @@ public class OrderController {
 
     @Resource
     private RestTemplate restTemplate;
+
+    @Resource
+    private LoadBalancer loadBalancer;
+
+    @Resource
+    private DiscoveryClient discoveryClient;
 
     @GetMapping("/consumer/payment/create")
     public CommonResult<Payment> create(Payment payment){
@@ -43,5 +54,19 @@ public class OrderController {
         }else{
             return new CommonResult<>(444,"Fail");
         }
+    }
+
+    @GetMapping("/consumer/payment/lb")
+    public String getPaymentLB()
+    {
+        List<ServiceInstance> instances = discoveryClient.getInstances("CLOUD-PAYMENT-SERVICE");
+
+        if(instances == null || instances.size()<=0) {
+            return null;
+        }
+        ServiceInstance serviceInstance = loadBalancer.instances(instances);
+        URI uri = serviceInstance.getUri();
+
+        return restTemplate.getForObject(uri+"/payment/lb",String.class);
     }
 }
---
+++ cloud-consumer-order80/src/main/java/com/sv/springcloud/lb/LoadBalancer.java
@@ -0,0 +1,8 @@
+package com.sv.springcloud.lb;
+
+import org.springframework.cloud.client.ServiceInstance;
+import java.util.List;
+
+public interface LoadBalancer {
+    ServiceInstance instances(List<ServiceInstance> serviceInstances);
+}
