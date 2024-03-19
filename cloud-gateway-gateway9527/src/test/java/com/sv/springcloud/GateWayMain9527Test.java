761185a0a343323cda8239c98f1b2b1e414dffed
owen-zhou20 <78416546+owen-zhou20@users.noreply.github.com>
2024-03-19 16:22:45 +1100

    feat: add SpringCloud GateWay.

---
+++ cloud-gateway-gateway9527/src/test/java/com/sv/springcloud/GateWayMain9527Test.java
@@ -0,0 +1,20 @@
+package com.sv.springcloud;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple GateWayMain9527.
+ */
+public class GateWayMain9527Test
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
