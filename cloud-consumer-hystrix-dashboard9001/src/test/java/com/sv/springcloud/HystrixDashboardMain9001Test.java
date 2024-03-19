1f262aeef1b2f6c0d0a1d487e72fcbf54e3f4d3e
owen-zhou20 <78416546+owen-zhou20@users.noreply.github.com>
2024-03-19 11:39:01 +1100

    feat: add Hystrix Dashboard.

---
+++ cloud-consumer-hystrix-dashboard9001/src/test/java/com/sv/springcloud/HystrixDashboardMain9001Test.java
@@ -0,0 +1,20 @@
+package com.sv.springcloud;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple HystrixDashboardMain9001.
+ */
+public class HystrixDashboardMain9001Test
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
