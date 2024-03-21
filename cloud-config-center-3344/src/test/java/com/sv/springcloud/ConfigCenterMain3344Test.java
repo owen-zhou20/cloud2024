bfdc6e64fdc9c1fa59defe6aeee032116fc1011a
owen-zhou20 <78416546+owen-zhou20@users.noreply.github.com>
2024-03-21 15:53:42 +1100

    feat: add Bus RabbitMQ.

---
+++ cloud-config-center-3344/src/test/java/com/sv/springcloud/ConfigCenterMain3344Test.java
@@ -0,0 +1,20 @@
+package com.sv.springcloud;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple ConfigCenterMain3344.
+ */
+public class ConfigCenterMain3344Test
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
---
+++ cloud-config-client-3366/src/test/java/com/sv/springcloud/ConfigClientMain3366Test.java
@@ -0,0 +1,20 @@
+package com.sv.springcloud;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple ConfigClientMain3366.
+ */
+public class ConfigClientMain3366Test
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
